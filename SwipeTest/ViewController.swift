a2858ee451978813ddbb7876d915591c48a97be7
Ayano Ohya <ayano.ohya@gmail.com>
2015-11-17 20:56:54 +0900

    Update some codes

--- SwipeTest/ViewController.swift
+++ SwipeTest/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     //TODO: PHAssetを使うとdeleteをした時にアラートが出てしまうので別のクラスを使う
     var photoAssets = [PHAsset]()
     
+    // 削除する写真を入れるための配列
+    var deletePhotoAssets = [PHAsset]()
+    
     var imageIndex: Int = 0
     
     var myImagePicker: UIImagePickerController!
@@ -79,7 +82,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     func swiperight(){
         NSLog("右にスワイプしました")
-        self.deleteFirstImage()
+        // self.deleteFirstImage()
+        self.addDeletePhotos()
     }
     func swipeup(){
         NSLog("上にスワイプしました")
@@ -117,7 +121,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     //アルバム表示
-    @IBAction func tapedPhotoBtn(sender: UIButton) {
+    @IBAction func tapedPhotoBtn() {
         if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary){
             // フォトライブラリの画像・写真選択画面を表示
             let imagePickerController = UIImagePickerController()
@@ -143,29 +147,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
 
-    
-    
-    /*
-    //画像が選択された時に呼ばれる.
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
-        
-        //選択された画像を取得.
-        var myImage: AnyObject?  = info[UIImagePickerControllerOriginalImage]
-        println(myImage)
-        
-//        //選択された画像を表示するViewControllerを生成.
-//        let secondViewController = SecondViewController()
-//        
-//        //選択された画像を表示するViewContorllerにセットする.
-//        secondViewController.mySelectedImage = myImage as! UIImage
-//        
-//        myImagePicker.pushViewController(secondViewController, animated: true)
-//        
-    }    /**
-
-*/
-    画像選択がキャンセルされた時に呼ばれる.
-    */
+    // 画像選択がキャンセルされた時に呼ばれる.
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         
         // モーダルビューを閉じる
@@ -173,170 +155,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
 
-
-    
-//    var imeage: UIImage = UIImage()
-//    
-//    var dataArray: [NSDate]=[]
-//    
-//    var photoArray:[NSData]=[]
-//    var photoNumber = 0
-    
-//    private func getAllPhotosInfo(){
-//        photoAssets = []
-//        
-//        var options = PHFetchOptions()
-//        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-//        
-//        
-//        //画像を全て取得
-//        var assets: PHFetchResult = PHAsset.fetchAssetsWithMediaType(.Image, options: nil)
-//        assets.enumerateObjectsUsingBlock { (asset,index, stop) -> Void in
-//            self.photoAssets.append(asset as! PHAsset)
-//            
-//        }
-//        println(photoAssets)
-//        let manager: PHImageManager = PHImageManager()
-//        manager.requestImageForAsset(photoAssets[imageIndex],targetSize: CGSizeMake(10, 10), contentMode: .AspectFit , options: nil) { (image, info) -> Void in
-//            
-//            //取得したimageをUIImageViewなどで表示する
-//            self.imageView!.image = image
-//           
-//        }
-//        
-//        
-//        
-//    }
-//    
-//    
-    
-//    
-//    @IBAction func tapedPhotoBtn(sender: UIButton) {
-//        
-//        // フォトライブラリが使用可能か？
-//        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
-//            
-//            // フォトライブラリの選択画面を表示
-//            let picker = UIImagePickerController()
-//            picker.delegate = self
-//            picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-//            self.presentViewController(picker, animated: true, completion: nil)
-//        }
-//        
-//    }
-    
-//    @IBAction func datePicker(sender: UITextField) {
-//        let datePickerView  : UIDatePicker = UIDatePicker()
-//        datePickerView.datePickerMode = UIDatePickerMode.Date
-//        sender.inputView = datePickerView
-//        datePickerView.addTarget(self, action: Selector("handleDatePicker:"), forControlEvents: UIControlEvents.ValueChanged)
-//    }
-//    
-//    func handleDatePicker(sender: UIDatePicker) {
-//        let dateFormatter = NSDateFormatter()
-//        dateFormatter.dateFormat = "yyyy MM dd"
-//        
-//        
-//    }
-//    
-//    func textFieldShouldReturn(textField: UITextField) -> Bool {
-//        textField.resignFirstResponder()
-//        
-//        return true
-//    }
-//    
-//    func selectBackground() {
-//        let imagePickerController: UIImagePickerController = UIImagePickerController()
-//        
-//        imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-//        imagePickerController.delegate = self
-//        imagePickerController.allowsEditing = true
-//        
-//        self.presentViewController(imagePickerController, animated: true, completion: nil)
-//    }
-//    
-//    func ImagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-//        
-//        image = info[UIImagePickerControllerEditedImage] as! UIImage
-//        
-//        imageView!.Image = image
-//        
-//        picker.dismissViewControllerAnimated(true, completion: nil)
-//        
-//    }
-    
-    
-    
-    
-    
-    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        
-//
-        // Do any additional setup after loading the view, typically from a nib.
-        
-//        self.addSwipeRecognizer()
-//        
-//        self.getAllPhotosInfo()
-//        
-//    }
-    
-    //
-    // --- photoSelectButton Delegate ---
-    //
-    
-//     func photoSelectButtonTouchDown(sender: AnyObject) {
-        
-//        
-//   func photoSelectButtonTouchDown(sender: AnyObject) {
-//        
-//        if !UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
-//            UIAlertView(title: "警告", message: "Photoライブラリにアクセス出来ません", delegate: nil, cancelButtonTitle: "OK").show()
-//        } else {
-//            var imagePickerController = UIImagePickerController()
-//            
-//            // フォトライブラリから選択
-//            imagePickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-//            
-//            // 編集OFFに設定
-//            // これをtrueにすると写真選択時、写真編集画面に移る
-//            imagePickerController.allowsEditing = false
-//            
-//            // デリゲート設定
-//            imagePickerController.delegate = self
-//            
-//            // 選択画面起動
-//            self.presentViewController(imagePickerController,animated:true ,completion:nil)
-//            
-////            // フォトライブラリの選択画面を表示
-//            let picker = UIImagePickerController()
-//            picker.delegate = self
-//            picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-//            self.presentViewController(picker, animated: true, completion: nil)
-//
-//        }
-//        
-//        
-//    }
-//    
-//    //
-//    // --- UIImagePickerDelegate ---
-//    //
-//    
-//    // 写真選択時に呼ばれる
-//    func imagePickerController(picker: UIImagePickerController!, didFinishPickingMediaWithInfo info: NSDictionary!) {
-//        
-//        // イメージ表示
-//        var image = info[UIImagePickerControllerOriginalImage] as! UIImage
-//        imageView.image = image
-//        
-//        // 選択画面閉じる
-//        self.dismissViewControllerAnimated(true, completion: nil)
-//    }
-//}
-//    
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -346,12 +164,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
 
     
-    private func deleteFirstImage() {
+    @IBAction func deleteImages() {
+        
+        // TODO: - 選んだ写真が入っているdeletePhotoAssets配列を一括で削除するコードに変える
         let delTargetAsset = photoAssets.first as PHAsset?
         if delTargetAsset != nil {
             PHPhotoLibrary.sharedPhotoLibrary().performChanges({ () -> Void in
                 // 削除などの変更はこのblocks内でリクエストする
-                PHAssetChangeRequest.deleteAssets([delTargetAsset!])
+                PHAssetChangeRequest.deleteAssets(self.deletePhotoAssets)
                 }, completionHandler: { (success, error) -> Void in
                     // 完了時の処理をここに記述
                    self.getAllPhotosInfo()
@@ -395,6 +215,11 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         return thumbnail
     }
     
+    func addDeletePhotos() {
+        // 今一番配列の上にある写真アセットを取得
+        let delTargetAsset = photoAssets.first as PHAsset?
+        deletePhotoAssets.append(delTargetAsset!)
+    }
     
     
     
